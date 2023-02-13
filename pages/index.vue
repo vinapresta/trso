ced1c39907df3edf7c14fd0823e5a51b6a17bf0f
unknown <vinapresta@gmail.com>
2023-02-13 10:16:47 +0100

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-        <PagesIndexFeatured />
+    <div>okok
+        <div v-for="item in data.data" :key="item.id">{{ item.id }}</div>
     </div>
 </template>
 
 <script setup>
 
+    const runtimeConfig = useRuntimeConfig()
 
+    const { data } = await useAsyncData('movies', () => $fetch('http://localhost:1337/api/movies'))
+
+    console.log('ok')
 
 </script>
