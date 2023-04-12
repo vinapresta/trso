c8969a928f42601a10c8f5b5c5f96b5c9640099b
unknown <vinapresta@gmail.com>
2023-04-12 17:12:51 +0200

    with output

---
+++ .output/public/_nuxt/error.39725006.js
@@ -0,0 +1,1 @@
+import{_ as e,d as r,b as c}from"./entry.01f0ab13.js";const o={};function t(n,s){return c(),r("div")}const a=e(o,[["render",t]]);export{a as default};
---
+++ .output/public/_nuxt/useAlgoliaInitIndex.7f70be31.js
@@ -0,0 +1,1 @@
+import{x as t}from"./entry.01f0ab13.js";const a=()=>t().$algolia;function i(o){const n=a();return n==null?void 0:n.initIndex(o)}export{i as u};
